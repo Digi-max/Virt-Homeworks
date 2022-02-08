5817e594f3def3a0983b45f96e1d51f097755f38
Digi-max <maxi-e@mail.ru>
2022-02-08 23:42:53 +0300

    task2

---
+++ 05-virt-05-docker-swarm/src/terraform_task2/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gl7gbj32u0kk1bcqaq"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gnqhlc481ecj41i1cq"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd86p2vpunojifv31s1f"
+}
