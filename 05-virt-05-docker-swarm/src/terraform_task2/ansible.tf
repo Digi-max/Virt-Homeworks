371adfe9bf02d3e2a02f4ed06b862ea014ad3d65
Digi-max <maxi-e@mail.ru>
2022-02-10 23:56:42 +0300

    task2 ansible

---
+++ 05-virt-05-docker-swarm/src/terraform_task2/ansible.tf
@@ -0,0 +1,21 @@
+resource "null_resource" "wait" {
+  provisioner "local-exec" {
+    command = "sleep 100"
+  }
+
+  depends_on = [
+    local_file.inventory
+  ]
+}
+
+resource "null_resource" "cluster" {
+  provisioner "local-exec" {
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/swarm-deploy-cluster.yml"
+  }
+
+  depends_on = [
+    null_resource.wait
+  ]
+}
+
+}
